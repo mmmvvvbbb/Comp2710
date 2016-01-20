988e6a1f44eb2134363b033a0f9d4e966773f68f
Matt Bonsall <Matt@Sandras-MacBook-Air.local>
2016-01-20 14:23:21 -0600

    Added Homework1v1

---
+++ hw1.cpp
@@ -0,0 +1,21 @@
+//Matthew Bonsall
+//MVB0005
+//Compiled using g++ compiler
+
+#include <iostream>
+using namespace std;
+
+const double RATIO = 0.001;
+
+int main() {
+	double lethal_amount, mouse_weight, your_weight, threshold;
+	cout << "What is the amount of artificial sweetener needed to kill a mouse: ";	
+	cin >> lethal_amount;
+	cout << "\nWhat is the weight of the mouse: ";
+	cin >> mouse_weight;
+	cout << "\nWhat is the weight of the dieter: ";
+	cin >> your_weight;
+	threshold = lethal_amount/RATIO * your_weight/mouse_weight;
+	cout << "\nYou can drink " << threshold << " diet soda without dying as a result.\n";
+	return 0;
+}

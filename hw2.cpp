75f882e1e9dc01f4f023f0bd2532034227923980
Matt Bonsall <Matt@Sandras-MacBook-Air.local>
2016-01-29 14:31:12 -0600

    Fixed formatting

--- hw2.cpp
+++ hw2.cpp
@@ -4,7 +4,17 @@
 //hw2.cpp
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+int doubleLength(double d){
+	int i = 3;
+	while (d > 1){
+		d /= 10;
+		i ++;
+	}
+	return i;
+}
 
 int main(){
 	double loan_amount, interest_rate;
@@ -21,19 +31,38 @@ int main(){
 	cout << "Monthly Payments: ";
 	cin >> month_pay;
 	
-	cout << "\n******************************************************";
-	cout << "\n\tAmortization Table";
-	cout << "\n******************************************************";
 
-	int cur_month = 0;
 	double month_interest, total_interest, principal;
+	int cur_month = 0;
 	total_interest = 0;
+	int balWidth = doubleLength(loan_amount)+5;
+	int payWidth = doubleLength(month_pay) +3;
+	if(payWidth < 10){
+		payWidth = 10;
+	}
+	
 	if(month_pay <= interest_rate*loan_amount/100){
 		cout << "\nThe owed interest is greater than the payment!\n";
 		return 0;
 	}
-	cout << "\nMonth\tBalance\tPayment\tRate\tInterest  Principal";
-	cout << "\n0\t$" << loan_amount << "\tN/A\tN/A\tN/A\tN/A";	
+	cout << "\n******************************************************";
+	cout << "\n\tAmortization Table";
+	cout << "\n******************************************************";
+	cout << endl<< left << setw(7) << "Month"; 
+	cout << setw(balWidth) << "Balance";
+	cout << setw(payWidth) << "Payment";
+	cout << setw(7)        << "Rate";
+	cout << setw(payWidth) << "Interest"; 
+	cout << setw(payWidth) << "Principal";
+	cout << setw(8)        << "\n0";
+	cout                   << "$"; 
+	cout << setw(balWidth-1) << loan_amount;
+	cout << setw(payWidth) << "N/A"; 
+	cout << setw(7)        << "N/A"; 
+	cout << setw(payWidth) << "N/A"; 
+	cout << setw(payWidth) << "N/A";	
+	payWidth--;
+	balWidth--;
 	while(loan_amount > 0){
 		month_interest =  loan_amount*interest_rate/100;
 		total_interest += month_interest;
@@ -43,12 +72,12 @@ int main(){
 			principal = loan_amount + principal;
 			loan_amount = 0;
 		}
-		cout << "\n" << ++cur_month << "\t";
-		cout << "$" << loan_amount << "\t";
-		cout << "$" << month_pay << "\t";
-		cout <<  interest_rate << "\t";
-		cout << "$" << month_interest << "\t";
-		cout << "$" << principal;
+		cout << "\n"<< setw(7) << ++cur_month;
+		cout << "$" << setw(balWidth) << loan_amount;
+		cout << "$" << setw(payWidth) << month_pay;
+		cout <<        setw(7) << interest_rate;
+		cout << "$" << setw(payWidth) << month_interest;
+		cout << "$" << setw(payWidth) << principal;
 		
 	}
 	cout << "\n******************************************************";

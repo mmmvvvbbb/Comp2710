831bccad1634c689b4ffee60fd2810c2e9576bc3
Matt Bonsall <matt.bonsall.v@gmail.com>
2016-02-08 12:45:35 -0600

    added hw4

---
+++ hw4.cpp
@@ -0,0 +1,18 @@
+//Matthew Bonsall
+//mvb0005
+//compiled using g++
+//hw3.cpp
+
+#include <fstream>
+#include <iostream>
+
+const int MAX_SIZE = 100;
+
+int readfile(int iArray[], ifstream& instream);
+
+int main(){
+
+	ifstream instream1;
+	
+}
+	
